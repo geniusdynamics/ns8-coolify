e3b5eefa657a3b878d9d6c66ce8402a9c71cc8c8
Martin Bhuong <martin@genius.ke>
2025-08-27 18:59:49 +0300

    Update to 4.0.0-beta.422

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="coolify"
-coolify_version="4.0.0-beta.421"
+coolify_version="4.0.0-beta.422"
 coolify_socketi="1.0.10"
 # Create a new empty container image
 container=$(buildah from scratch)
